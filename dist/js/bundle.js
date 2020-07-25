02d990916e413495e2700072576a9bab146a69bc
centerRyo <abc.atrn@gmail.com>
2020-07-25 15:20:16 +0900

    ローカルストレージAPIを使用するための記述を追加した

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -293,6 +293,22 @@ var _vue2 = _interopRequireDefault(_vue);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// ローカルストレージAPIを使用する
+var STORAGE_KEY = 'todo_vue';
+var todoStorage = {
+  fetch: function fetch() {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    todos.forEach(function (todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function save(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
+
 new _vue2.default({
   el: '#app',
   data: {
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,23 @@
 import Vue from 'vue';
 
+// ローカルストレージAPIを使用する
+var STORAGE_KEY = 'todo_vue';
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    );
+    todos.forEach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+}
+
 new Vue({
   el: '#app',
   data: {
